1ad500e3839945b742797f623fc4f4502045d3f5
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-27 13:51:38 -0500

    Gulp, bower, package.json y gitignore añadidos

--- lib/App/Controller/Example.pm
+++ lib/App/Controller/Example.pm
@@ -1,12 +1,9 @@
 package App::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller';
 
-# This action will render a template
 sub welcome {
-  my $self = shift;
-
-  # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+    my $self = shift;
+    $self->render(template => 'example/welcome', msg => 'Welcome to the Mojolicious real-time web framework!');
 }
 
 1;
