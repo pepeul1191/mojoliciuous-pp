17c181cb3cfdd23f13d0ae26089296ce7b9bfd8c
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-28 09:25:56 -0500

    Data parseada en JSON del controlador a la vista

--- lib/App/Controller/Login.pm
+++ lib/App/Controller/Login.pm
@@ -6,14 +6,14 @@ use App::Config::Variables;
 sub index {
     my $self = shift;
     my %data = (
-        
+        mensaje => true
     ); 
     my %helper = (
         title => 'Bienvenido', 
         css => 'dist/login.min.css',
         js_top => 'http://localhost:3000/',
         js_bottom => 'dist/login.min.js',
-        data => '{"mensaje":false}',
+        data => encode_json \%data,
     );
     $self->render(template => 'login/index', variables => \%App::Config::Variables::Data, helper => \%helper);
 }
