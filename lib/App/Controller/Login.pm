667d72f8e3cac805a8ffcb6c74f7f658f21bf88a
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-27 19:14:58 -0500

    Avance del login

--- lib/App/Controller/Login.pm
+++ lib/App/Controller/Login.pm
@@ -4,7 +4,13 @@ use App::Config::Variables;
 
 sub index {
     my $self = shift;
-    $self->render(template => 'login/index', variables => \%App::Config::Variables::Data);
+    my %helper = (
+        title => 'Bienvenido', 
+        css => 'dist/login.min.css',
+        js_top   => 'http://localhost:3000/',
+        js_bottom    => 'dist/login.min.js',
+    );
+    $self->render(template => 'login/index', variables => \%App::Config::Variables::Data, helper => \%helper);
 }
 
 1;
