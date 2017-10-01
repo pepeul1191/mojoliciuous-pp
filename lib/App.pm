a876c73b9ddc5ac3cc70e9f9292751d2ba2fb2b2
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-30 19:03:51 -0500

    Before filter antes creado ahora en otro archivo (Mojo::Plugin)

--- lib/App.pm
+++ lib/App.pm
@@ -15,23 +15,15 @@ sub startup {
     });
     # Documentation browser under "/perldoc"
     $self->plugin('PODRenderer');
+    # Plugin
+    $self->plugin('App::Plugin::SessionTrue');
     # Router
     my $r = $self->routes;
     # conditions
-    $r->add_condition(session => sub {
-        my ($route, $c, $captures, $session) = @_;
-         $log->debug('1 ++++++++++++++++++++++++++++++++++++++++++++');
-        $log->debug('Not sure what is happening here');
-        $log->debug('Not sure what is happening here');
-         if ($session == 1){
-            $c->redirect_to(%App::Config::Variables::Data{'BASE_URL'} . 'home');
-         }
-        $log->debug('2 ++++++++++++++++++++++++++++++++++++++++++++');
-        return 1;
-    });
+
     # Normal route to controller
     $r->get('/')->to('example#welcome');
-    $r->get('/login')->over(session => 0, session => 1)->to('login#index');
+    $r->get('/login')->over(sesion_true => 0, sesion_true => 1)->to('login#index');
     $r->get('/salir')->to('login#salir');
     $r->post('/login/acceder')->to('login#acceder');
 }
---
+++ lib/App/Plugin/SessionTrue.pm
@@ -0,0 +1,21 @@
+package App::Plugin::SessionTrue;
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::Log;
+
+my $log = Mojo::Log->new;
+
+sub register {
+  my ($self, $app) = @_;
+
+  # Add "werewolf" condition
+  $app->routes->add_condition(sesion_true => sub {
+    my ($route, $c, $captures, $session) = @_;
+         if ($session == 1){
+            $c->redirect_to(%App::Config::Variables::Data{'BASE_URL'} . 'home');
+         }
+        #$log->debug('2 ++++++++++++++++++++++++++++++++++++++++++++');
+        return 1;
+  });
+}
+
+1;
