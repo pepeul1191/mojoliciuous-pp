fa532da3aa2cb45c3f3bb9f6e43414977de162da
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-10 11:34:57 -0500

    CRUD de Permisos con UTF8

--- lib/App/Controller/Acceso/Permiso.pm
+++ lib/App/Controller/Acceso/Permiso.pm
@@ -5,6 +5,8 @@ use JSON;
 use Mojo::Log;
 use REST::Client;
 use utf8;
+use Encode qw(decode encode);
+binmode STDOUT, ':utf8';
 
 my $log = Mojo::Log->new;
 
@@ -12,8 +14,8 @@ sub listar {
     my $self = shift;
     my $sistema_id = $self->param('sistema_id');
     my $url = %App::Config::Variables::Data{'accesos'} . 'permiso/listar/' . $sistema_id;
-	my $client = REST::Client->new(); $client->GET($url);
-    my $rpta = $client->responseContent();
+    my $client = REST::Client->new(); $client->GET($url);
+    my $rpta = decode('utf8', $client->responseContent());
 
     $self->render(text => $rpta);
 }
@@ -23,8 +25,8 @@ sub listar_asociados {
     my $sistema_id = $self->param('sistema_id');
     my $rol_id = $self->param('rol_id');
     my $url = %App::Config::Variables::Data{'accesos'} . 'permiso/listar_asociados/' . $sistema_id . '/' . $rol_id;
-	my $client = REST::Client->new(); $client->GET($url);
-    my $rpta = $client->responseContent();
+    my $client = REST::Client->new(); $client->GET($url);
+    my $rpta = decode('utf8', $client->responseContent());
 
     $self->render(text => $rpta);
 }
@@ -34,7 +36,7 @@ sub guardar {
     my $data = $self->param('data');
     my $url = %App::Config::Variables::Data{'accesos'} . 'permiso/guardar?data=' . $data;
     my $client = REST::Client->new(); $client->POST($url);
-    my $rpta = $client->responseContent();
+    my $rpta = decode('utf8', $client->responseContent());
 
     $self->render(text => $rpta);
 }
